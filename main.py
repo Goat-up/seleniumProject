f70f0f6a1fb306f6560861de49fc0c4929cf09d3
zzh <2397085590@qq.com>
2022-07-12 16:37:10 +0800

    face to Object program

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TreeChildSelect:
+
+    def __init__(self, driver2):
+        print("TreeChildSelect")
+        driver2.implicitly_wait(3)
+        driver2.get('https://dj.gzmky.cn/cmscp/index.do')
+
+    @staticmethod
+    def selectTreeChild(driver1):
+        aElements = driver1.find_elements(By.CSS_SELECTOR, '.sidebar-menu .treeview a')
+        # 点击功能
+        aElements[46].click()
+
+    @staticmethod
+    def loginTest(driver3, username, password):
+        inputBox1 = driver3.find_element(By.ID, 'username')
+        inputBox2 = driver3.find_element(By.ID, 'password')
+        button1 = driver3.find_element(By.TAG_NAME, 'button')
+        inputBox1.send_keys(username)
+        inputBox2.send_keys(password)
+        button1.click()
+
+
 
--- test.py
+++ test.py
@@ -5,11 +5,15 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
-# wd = webdriver.Chrome()
-wd = webdriver.Firefox()
+wd = webdriver.Chrome()
+# wd = webdriver.Firefox()
 # # 等待元素渲染
 wd.implicitly_wait(5)
 wd.get('https://dj.gzmky.cn/cmscp/index.do')
+
+time.sleep(1)
+wd.maximize_window()
+time.sleep(1)
 try:
     # 登录
     inputBox1 = wd.find_element(By.ID, 'username')
@@ -20,7 +24,7 @@ try:
     button1.click()
     # 获取所有菜单
     aElements = wd.find_elements(By.CSS_SELECTOR, '.sidebar-menu .treeview a')
-    # 从上到下依次点击所有功能
+    # 点击所有功能
     aElements[46].click()
     time.sleep(1)
     aElements[48].click()
@@ -29,10 +33,29 @@ try:
     pswElements = wd.find_elements(By.CSS_SELECTOR, '.row input')
     for pswElement in pswElements:
         pswElement.send_keys("123456")
+    time.sleep(1)
     wd.back()
-except NoSuchElementException:
-    pass
 
+    aElements[40].click()
+    aElements[41].click()
+    # 树形组织-测试组
+    wd.switch_to.frame("leftFrame")
+    wd.find_element(By.ID, "tree_41_switch").click()
+    wd.find_element(By.ID, "tree_43_span").click()
+    # 组织列表搜索框
+
+    wd.switch_to.default_content()
+
+    wd.switch_to.frame("centerFrame")
+    searchBox = wd.find_element(By.CSS_SELECTOR, "body > div.content > div > div > form.form-inline.ls-search > div:nth-child(3) > input")
+    searchButton = wd.find_element(By.CSS_SELECTOR, "body > div.content > div > div > form.form-inline.ls-search > button")
+    searchBox.send_keys("123")
+    time.sleep(1)
+    searchButton.click()
+    time.sleep(1)
+except NoSuchElementException:
+    a = "123"
+    print("a=%s" % a)
 
 
 
